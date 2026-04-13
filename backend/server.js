7d18d13f02ce2c419ff910d618a5d1326a4a1e11
ov514 <ov5347304@gmail.com>
2026-04-14 03:32:33 +0700

    Configure Vercel Backend

--- backend/server.js
+++ backend/server.js
@@ -95,8 +95,10 @@ app.get('/', (req, res) => {
     res.send('AURA API is running...');
 });
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
+export default app;
